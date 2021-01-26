a087bddcdc9255e3cfe81787bbfd3d2f542fd230
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-01-26 21:53:37 +0900

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config({ path: './.env'});
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+app.listen(port, () => console.log('server listening on port', port));
