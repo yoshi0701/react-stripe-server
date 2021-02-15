4fb5eb6ae72b7e6994f8772878e12b3c7d2e0920
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-02-16 02:24:56 +0900

    feat: stripe create checkout session endpoint implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config({ path: './.env'});
+const createCheckoutSession = require('./api/checkout');
 
 const app = express();
 const port = 8080;
@@ -10,5 +11,7 @@ app.use(cors({ origin: true }));
 
 
 app.get('/', (req, res) => res.send('Hello world'));
+
+app.post('/create-checkout-session', createCheckoutSession);
 
 app.listen(port, () => console.log('server listening on port', port));
