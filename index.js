bcaaceaff09f6681e2fd80d12993254c80655d5b
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-02-20 22:16:20 +0900

    feat: payment intent config added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config({ path: './.env'});
 const createCheckoutSession = require('./api/checkout');
 const webhook = require('./api/webhook');
+const paymentIntent = require('./api/paymentIntent');
 
 const app = express();
 const port = 8080;
@@ -18,5 +19,7 @@ app.get('/', (req, res) => res.send('Hello world'));
 app.post('/create-checkout-session', createCheckoutSession);
 
 app.post('/webhook', webhook);
+
+app.post('/create-payment-intent', paymentIntent);
 
 app.listen(port, () => console.log('server listening on port', port));
